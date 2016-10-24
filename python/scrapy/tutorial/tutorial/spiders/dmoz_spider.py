d9cce43b6ec1fe85c705c4a1a6fa6a975c911684
wdd <wdd@ubuntu.(none)>
2016-10-24 17:04:13 +0800

    feature:add learning for R and scrapy(python)

---
+++ python/scrapy/tutorial/tutorial/spiders/dmoz_spider.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding=utf-8
+import scrapy
+from tutorial.items import DmozItem
+class DmozSpider(scrapy.Spider):
+    name = "dmoz"
+    allowed_domains = ["dmoz.org"]
+    start_urls = [
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/",
+    ]
+    '''
+    start_urls = [
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+    ]
+    
+    def parse(self, response):
+        for sel in response.xpath('//ul/li'):
+            item = DmozItem()
+            item['title'] = sel.xpath('a/text()').extract()
+            item['link'] = sel.xpath('a/@href').extract()
+            item['desc'] = sel.xpath('text()').extract()
+            yield item
+
+        filename = response.url.split("/")[-2]
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+    '''
+    def parse(self, response):
+        for href in response.xpath("//div/section[2]/div/section/div/div/a/@href"):
+            url = response.urljoin(href.extract())
+            yield scrapy.Request(url, callback=self.parse_dir_contents)
+
+    def parse_dir_contents(self, response):
+        for sel in response.xpath('//ul/li'):
+            item = DmozItem()
+            item['title'] = sel.xpath('a/text()').extract()
+            item['link'] = sel.xpath('a/@href').extract()
+            item['desc'] = sel.xpath('text()').extract()
+            yield item
